c13432ab3f76a014f1bfb74dc3c7bc21e1117aa3
Charles Leifer <coleifer@gmail.com>
2010-08-21 23:22:04 -0500

    Adding a google bot as an example

---
+++ bots/google.py
@@ -0,0 +1,36 @@
+import httplib2
+import json
+import urllib
+
+from irc import Dispatcher, IRCBot
+
+
+class GoogleDispatcher(Dispatcher):
+    def fetch_result(self, query):
+        sock = httplib2.Http(timeout=1)
+        headers, response = sock.request(
+            'http://ajax.googleapis.com/ajax/services/search/web?v=2.0&q=%s' % \
+            urllib.quote(query)
+        )
+        if headers['status'] in (200, '200'):
+            response = json.loads(response)
+            return response['responseData']['results'][0]['unescapedUrl']
+    
+    def greet(self, sender, message, channel, is_ping, reply):
+        if is_ping:
+            result = self.fetch_result(message.replce('find me ', '')
+            if result:
+                reply(result)
+    
+    def get_patterns(self):
+        return (
+            ('^find me \S+', self.greet),
+        )
+
+
+host = 'irc.freenode.net'
+port = 6667
+nick = 'googlebot'
+
+greeter = IRCBot(host, port, nick, ['#lawrence-botwars'], [GoogleDispatcher])
+greeter.run_forever()
