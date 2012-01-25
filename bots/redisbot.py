482d5530fc0a71a55de89e2448ce8d9a2a4274ce
Charles Leifer <coleifer@gmail.com>
2012-01-24 20:34:26 -0600

    Rewriting the markov bot to use redis

---
+++ bots/redisbot.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python
+import random
+import re
+import redis
+
+from irc import IRCBot, run_bot
+
+
+class MarkovBot(IRCBot):
+    """
+    http://code.activestate.com/recipes/194364-the-markov-chain-algorithm/
+    http://github.com/ericflo/yourmomdotcom
+    """
+    chain_length = 2
+    chattiness = .01
+    max_words = 30
+    messages_to_generate = 5
+    prefix = 'irc'
+    separator = '\x01'
+    stop_word = '\x02'
+    
+    def __init__(self, *args, **kwargs):
+        super(MarkovBot, self).__init__(*args, **kwargs)
+        
+        self.redis_conn = redis.Redis()
+    
+    def make_key(self, k):
+        return '-'.join((self.prefix, k))
+    
+    def sanitize_message(self, message):
+        return re.sub('[\"\']', '', message.lower())
+
+    def split_message(self, message):
+        # split the incoming message into words, i.e. ['what', 'up', 'bro']
+        words = message.split()
+        
+        # if the message is any shorter, it won't lead anywhere
+        if len(words) > self.chain_length:
+            
+            # add some stop words onto the message
+            # ['what', 'up', 'bro', '\n', '\n']
+            words.extend([self.stop_word] * self.chain_length)
+            
+            # len(words) == 5, so range(5-2-1) == range(2) == 0, 1, meaning
+            # we return the following slices: [0:3], [1:4]
+            # or ['what', 'up', 'bro'], ['up', 'bro', '\x02']
+            for i in range(len(words) - self.chain_length - 1):
+                yield words[i:i + self.chain_length + 1]
+    
+    def generate_message(self, seed):
+        message = []
+        
+        # generate a certain number of messages and return the longest
+        for i in range(self.messages_to_generate):
+            key = seed
+            
+            gen_words = []
+            for i in xrange(self.max_words):
+                # split the key on the separator to extract the words
+                words = key.split(self.separator)
+                
+                # if we've hit a stop-word, break here
+                if words[0] == self.stop_word:
+                    break
+
+                # add the word to the list of words in our generated message
+                gen_words.append(words[0])
+                
+                # get a new word
+                next_word = self.redis_conn.srandmember(self.make_key(key))
+                if not next_word:
+                    break
+                
+                key = self.separator.join(words[1:] + [next_word])
+
+            if len(gen_words) > len(message):
+                message = list(gen_words)
+        
+        return ' '.join(message)
+
+    def log(self, sender, message, channel):
+        if message.startswith('/'):
+            return
+
+        # speak only when spoken to, or when the spirit moves me
+        say_something = self.is_ping(message) or (
+            sender != self.conn.nick and random.random() < self.chattiness
+        )
+        
+        messages = []
+        seed = None
+        
+        # use a convenience method to strip out the "ping" portion of a message
+        if self.is_ping(message):
+            message = self.fix_ping(message)
+
+        # split up the incoming message into chunks that are 1 word longer than
+        # the size of the chain, e.g. ['what', 'up', 'bro'], ['up', 'bro', '\x02']
+        for words in self.split_message(self.sanitize_message(message)):
+            # grab everything but the last word
+            key = self.separator.join(words[:-1])
+            
+            # add the last word to the set
+            self.redis_conn.sadd(self.make_key(key), words[-1])
+            
+            # if we should say something, generate some messages based on what
+            # was just said
+            if say_something:
+                generated = self.generate_message(seed=key)
+                if generated:
+                    messages.append(generated)
+        
+        if len(messages):
+            return random.choice(messages)
+
+    def command_patterns(self):
+        return (
+            ('.*', self.log),
+        )
+
+
+host = 'irc.freenode.net'
+port = 6667
+nick = 'whatyousay2'
+
+run_bot(MarkovBot, host, port, nick, ['#lawrence-botwars'])
