227a3a45ea2f7b9825f5aa2ef6ae68ca7d5bc2d0
Charles Leifer <coleifer@gmail.com>
2013-06-20 12:41:38 -0500

    Small fix to joinre

--- irc.py
+++ irc.py
@@ -25,51 +25,51 @@ class IRCConnection(object):
     chanmsg_re = re.compile(':(?P<nick>.*?)!\S+\s+?PRIVMSG\s+(?P<channel>#+[-\w]+)\s+:(?P<message>[^\n\r]+)')
     privmsg_re = re.compile(':(?P<nick>.*?)!~\S+\s+?PRIVMSG\s+[^#][^:]+:(?P<message>[^\n\r]+)')
     part_re = re.compile(':(?P<nick>.*?)!\S+\s+?PART\s+(?P<channel>#+[-\w]+)')
-    join_re = re.compile(':(?P<nick>.*?)!\S+\s+?JOIN\s+:\s*(?P<channel>#+[-\w]+)')
+    join_re = re.compile(':(?P<nick>.*?)!\S+\s+?JOIN\s+.*?(?P<channel>#+[-\w]+)')
     quit_re = re.compile(':(?P<nick>.*?)!\S+\s+?QUIT\s+.*')
     registered_re = re.compile(':(?P<server>.*?)\s+(?:376|422)')
-    
+
     # mapping for logging verbosity
     verbosity_map = {
         0: logging.ERROR,
         1: logging.INFO,
         2: logging.DEBUG,
     }
-    
+
     def __init__(self, server, port, nick, logfile=None, verbosity=1, needs_registration=True):
         self.server = server
         self.port = port
         self.nick = self.base_nick = nick
-        
+
         self.logfile = logfile
         self.verbosity = verbosity
-        
+
         self._registered = not needs_registration
         self._out_buffer = []
         self._callbacks = []
         self.logger = self.get_logger('ircconnection.logger', self.logfile)
-    
+
     def get_logger(self, logger_name, filename):
         log = logging.getLogger(logger_name)
         log.setLevel(self.verbosity_map.get(self.verbosity, logging.INFO))
-        
+
         if self.logfile:
             handler = RotatingFileHandler(filename, maxBytes=1024*1024, backupCount=2)
             handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
             log.addHandler(handler)
-        
+
         if self.verbosity == 2 or not self.logfile:
             stream_handler = logging.StreamHandler()
             stream_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
             log.addHandler(stream_handler)
-        
+
         return log
-    
+
     def send(self, data, force=False):
         """\
         Send raw data over the wire if connection is registered. Otherewise,
         save the data to an output buffer for transmission later on.
-        If the force flag is true, always send data, regardless of 
+        If the force flag is true, always send data, regardless of
         registration status.
         """
         if self._registered or force:
@@ -77,7 +77,7 @@ class IRCConnection(object):
             self._sock_file.flush()
         else:
             self._out_buffer.append(data)
-    
+
     def connect(self):
         """\
         Connect to the IRC server using the nickname
@@ -88,14 +88,14 @@ class IRCConnection(object):
         except socket.error:
             self.logger.error('Unable to connect to %s on port %d' % (self.server, self.port), exc_info=1)
             sys.exit(1)
-        
+
         self._sock_file = self._sock.makefile()
         self.register_nick()
         self.register()
-    
+
     def close(self):
-        self._sock.close()    
-    
+        self._sock.close()
+
     def register_nick(self):
         self.logger.info('Registering nick %s' % self.nick)
         self.send('NICK %s' % self.nick, True)
@@ -127,12 +127,12 @@ class IRCConnection(object):
             self.send('PRIVMSG %s :%s' % (channel, message))
         elif nick:
             self.send('PRIVMSG %s :%s' % (nick, message))
-    
+
     def dispatch_patterns(self):
         """\
         Low-level dispatching of socket data based on regex matching, in general
         handles
-        
+
         * In event a nickname is taken, registers under a different one
         * Responds to periodic PING messages from server
         * Dispatches to registered callbacks when
@@ -151,13 +151,13 @@ class IRCConnection(object):
             (self.privmsg_re, self.handle_private_message),
             (self.registered_re, self.handle_registered),
         )
-    
+
     def register_callbacks(self, callbacks):
         """\
         Hook for registering custom callbacks for dispatch patterns
         """
         self._callbacks.extend(callbacks)
-    
+
     def new_nick(self):
         """\
         Generates a new nickname based on original nickname followed by a
@@ -192,42 +192,42 @@ class IRCConnection(object):
             for data in self._out_buffer:
                 self.send(data)
             self._out_buffer = []
-    
+
     def handle_part(self, nick, channel):
         for pattern, callback in self._callbacks:
             if pattern.match('/part'):
                 callback(nick, '/part', channel)
-    
+
     def handle_join(self, nick, channel):
         for pattern, callback in self._callbacks:
             if pattern.match('/join'):
                 callback(nick, '/join', channel)
-    
+
     def handle_quit(self, nick):
         for pattern, callback in self._callbacks:
             if pattern.match('/quit'):
                 callback(nick, '/quit', None)
-    
+
     def _process_command(self, nick, message, channel):
         results = []
-        
+
         for pattern, callback in self._callbacks:
             match = pattern.match(message) or pattern.match('/privmsg')
             if match:
                 results.append(callback(nick, message, channel, **match.groupdict()))
-        
+
         return results
-    
+
     def handle_channel_message(self, nick, channel, message):
         for result in self._process_command(nick, message, channel):
             if result:
                 self.respond(result, channel=channel)
-    
+
     def handle_private_message(self, nick, message):
         for result in self._process_command(nick, message, None):
             if result:
                 self.respond(result, nick=nick)
-    
+
     def enter_event_loop(self):
         """\
         Main loop of the IRCConnection - reads from the socket and dispatches
@@ -235,18 +235,18 @@ class IRCConnection(object):
         """
         patterns = self.dispatch_patterns()
         self.logger.debug('entering receive loop')
-        
+
         while 1:
             try:
                 data = self._sock_file.readline()
             except socket.error:
                 data = None
-            
+
             if not data:
                 self.logger.info('server closed connection')
                 self.close()
                 return True
-            
+
             data = data.rstrip()
 
             for pattern, callback in patterns:
@@ -262,10 +262,10 @@ class IRCBot(object):
     """
     def __init__(self, conn):
         self.conn = conn
-        
+
         # register callbacks with the connection
         self.register_callbacks()
-    
+
     def register_callbacks(self):
         """\
         Hook for registering callbacks with connection -- handled by __init__()
@@ -274,36 +274,36 @@ class IRCBot(object):
             (re.compile(pattern), callback) \
                 for pattern, callback in self.command_patterns()
         ))
-    
+
     def _ping_decorator(self, func):
         def inner(nick, message, channel, **kwargs):
             message = re.sub('^%s[:,\s]\s*' % self.conn.nick, '', message)
             return func(nick, message, channel, **kwargs)
         return inner
-    
+
     def is_ping(self, message):
         return re.match('^%s[:,\s]' % self.conn.nick, message) is not None
-    
+
     def fix_ping(self, message):
         return re.sub('^%s[:,\s]\s*' % self.conn.nick, '', message)
-    
+
     def ping(self, pattern, callback):
         return (
             '^%s[:,\s]\s*%s' % (self.conn.nick, pattern.lstrip('^')),
             self._ping_decorator(callback),
         )
-    
+
     def command_patterns(self):
         """\
         Hook for defining callbacks, stored as a tuple of 2-tuples:
-        
+
         return (
             ('/join', self.room_greeter),
             ('!find (^\s+)', self.handle_find),
         )
         """
         raise NotImplementedError
-    
+
     def respond(self, message, channel=None, nick=None):
         """\
         Wraps the connection object's respond() method
@@ -318,26 +318,26 @@ def run_bot(bot_class, host, port, nick, channels=None):
     """
     conn = IRCConnection(host, port, nick)
     bot_instance = bot_class(conn)
-    
+
     while 1:
         conn.connect()
-        
+
         channels = channels or []
-        
+
         for channel in channels:
             conn.join(channel)
-        
+
         conn.enter_event_loop()
 
 
 class SimpleSerialize(object):
     """\
     Allow simple serialization of data in IRC messages with minimum of space.
-    
+
     * Only supports dictionaries *
     """
     def serialize(self, dictionary):
         return '|'.join(('%s:%s' % (k, v) for k, v in dictionary.iteritems()))
-    
+
     def deserialize(self, string):
         return dict((piece.split(':', 1) for piece in string.split('|')))
